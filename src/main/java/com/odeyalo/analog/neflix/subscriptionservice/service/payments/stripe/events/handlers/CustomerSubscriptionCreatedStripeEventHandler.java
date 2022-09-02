22b2b4c210081b42242c4ccc347397ac21d3b264
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-02 16:05:07 +0300

    Abstract class that implements some methods

---
+++ src/main/java/com/odeyalo/analog/neflix/subscriptionservice/service/payments/stripe/events/handlers/CustomerSubscriptionCreatedStripeEventHandler.java
@@ -0,0 +1,23 @@
+package com.odeyalo.analog.neflix.subscriptionservice.service.payments.stripe.events.handlers;
+
+import com.odeyalo.analog.neflix.subscriptionservice.service.payments.stripe.events.handlers.support.StripeEventHandlerAutoRegistry;
+import com.stripe.model.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class CustomerSubscriptionCreatedStripeEventHandler implements StripeEventHandler, StripeEventHandlerAutoRegistry {
+    protected final Logger logger = LoggerFactory.getLogger(CustomerSubscriptionCreatedStripeEventHandler.class);
+
+    @Override
+    public abstract void handleEvent(Event event);
+
+    @Override
+    public String getEventType() {
+        return "customer.subscription.created";
+    }
+
+    @Override
+    public StripeEventHandler getHandler() {
+        return this;
+    }
+}
