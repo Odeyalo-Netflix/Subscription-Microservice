18f4ea87779725da4602a98e559c2dedc66a7e77
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-02 16:26:54 +0300

    Simple registry interface that registry a class in some factory or container

---
+++ src/main/java/com/odeyalo/analog/neflix/subscriptionservice/service/payments/stripe/events/StripeEventHandlerExecutorStrategyAutoRegistry.java
@@ -0,0 +1,20 @@
+package com.odeyalo.analog.neflix.subscriptionservice.service.payments.stripe.events;
+
+/**
+ * Simple registry interface that registry a class in some factory, like StripeEventHandlerExecutorStrategyFactory or registry to Kafka container
+ * It can be called like markable interface that simple contains metadata that will be used during registration in container
+ * @see StripeEventHandlerExecutorStrategyFactory
+ */
+public interface StripeEventHandlerExecutorStrategyAutoRegistry {
+    /**
+     * An Strategy type. Key of this strategy
+     * @return - an strategy name
+     */
+    String getStrategyKey();
+
+    /**
+     * Strategy that will be registered with specific key. It can be called Value
+     * @return - an StripeEventHandlerExecutorStrategy to registry
+     */
+    StripeEventHandlerExecutorStrategy getStrategy();
+}
